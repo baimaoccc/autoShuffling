d5710a1b22fcae3de7798a4b77c4917dc22ef0ea
baimao <1099382073@qq.com>
2017-08-04 16:13:51 +0800

    面向对象版本自动轮播

--- js/jquery.zyslide.js
+++ js/jquery.zyslide.js
@@ -1,141 +1,134 @@
 (function($) {
+	//创建构造方法
+	function Slide(ele,options) {
+	    var setting = {
+            // 控制炸开的时间
+            delay: 1000,
+            // 控制轮播图的速度
+            speed: 2000
+        };
+	    this.ele = ele;
+	    this.interval = null;
+	    this.setting = (options ? ($.extend(true, setting, options)) : setting);
+	    this.states = [
+            {
+                ZIndex: 1,
+                width: 120,
+                height: 150,
+                top: 75,
+                left: 100,
+                ZOpacity: 0.2
+            },
+            {
+                ZIndex: 2,
+                width: 130,
+                height: 170,
+                top: 65,
+                left: 50,
+                ZOpacity: 0.45
+            },
+            {
+                ZIndex: 3,
+                width: 150,
+                height: 200,
+                top: 50,
+                left: 140,
+                ZOpacity: 0.7
+            },
+            {
+                ZIndex: 4,
+                width: 200,
+                height: 300,
+                top: 0,
+                left: 275,
+                ZOpacity: 1
+            },
+            {
+                ZIndex: 3,
+                width: 150,
+                height: 200,
+                top: 50,
+                left: 460,
+                ZOpacity: 0.7
+            },
+            {
+                ZIndex: 2,
+                width: 130,
+                height: 170,
+                top: 65,
+                left: 565,
+                ZOpacity: 0.45
+            },
+            {
+                ZIndex: 1,
+                width: 120,
+                height: 150,
+                top: 75,
+                left: 450,
+                ZOpacity: 0.2
+            }
+        ];
+	    this.lis = $(this.ele).find('li');
+    }
 
-	// 本函数每次调用只负责一个轮播图的功能
-	// 也就是说只会参数一个轮播图，这个函数的作用域只能
-	// 分配一个轮播图， ele 是某个轮播图的根标签
-	var slide = function(ele,options) {
-		// 将 ele 转换成 jquery 标签对象
-		var $ele = $(ele);
-		
-		// 默认设置的选项
-		var setting = {
-			// 控制炸开的时间
-			delay: 1000,
-			// 控制轮播图的速度
-			speed: 2000
-		}
-		// 对象合并
-		$.extend(true, setting, options);
-		
-		// 规定好每张图片处于的位置和状态
-		var states = [{
-				ZIndex: 1,
-				width: 120,
-				height: 150,
-				top: 75,
-				left: 100,
-				ZOpacity: 0.2
-			},
-			{
-				ZIndex: 2,
-				width: 130,
-				height: 170,
-				top: 65,
-				left: 50,
-				ZOpacity: 0.45
-			},
-			{
-				ZIndex: 3,
-				width: 150,
-				height: 200,
-				top: 50,
-				left: 140,
-				ZOpacity: 0.7
-			},
-			{
-				ZIndex: 4,
-				width: 200,
-				height: 300,
-				top: 0,
-				left: 275,
-				ZOpacity: 1
-			},
-			{
-				ZIndex: 3,
-				width: 150,
-				height: 200,
-				top: 50,
-				left: 460,
-				ZOpacity: 0.7
-			},
-			{
-				ZIndex: 2,
-				width: 130,
-				height: 170,
-				top: 65,
-				left: 565,
-				ZOpacity: 0.45
-			},
-			{
-				ZIndex: 1,
-				width: 120,
-				height: 150,
-				top: 75,
-				left: 450,
-				ZOpacity: 0.2
-			}
-		]
+    Slide.prototype = {
+        move: function () {
+            var _this = this;
+            this.lis.each(function(index, ele) {
+                var state = _this.states[index];
 
-		var lis = $ele.find('li');
-		// 让每个 li 对应 上面设置的 states 的每个状态
-		function move() {
-			lis.each(function(index, ele) {
-				var state = states[index];
+                $(ele).css('z-index', state.ZIndex).finish().animate(state, _this.setting.delay).find('img').css('opacity', state.ZOpacity);
+            })
+        },
+        next: function () {
+            this.states.unshift(this.states.pop());
+            console.log(this.states);
+            this.move();
+        },
+        prev: function () {
+            this.states.push(this.states.shift());
+            this.move();
+        },
+        autoPlay: function () {
+            var _this = this;
+            this.interval = setInterval(function() {
+                _this.next();
+            }, _this.setting.speed);
+        },
+        showShuffling: function () {
+            var _this = this;
+            this.move();
+            this.autoPlay();
+            // 点击下一张
+            $(this.ele).find('.zy-next').click(function() {
+                _this.next()
+            });
+            // 点击上一张
+            $(this.ele).find('.zy-prev').click(function() {
+                _this.prev()
+            });
+            // 前后按钮的hover事件
+            $(this.ele).find('section').hover(function() {
+                clearInterval(_this.interval);
+            }, function() {
+                _this.autoPlay();
+            });
+        }
+    }
 
-				$(ele).css('z-index', state.ZIndex).finish().animate(state, setting.delay).find('img').css('opacity', state.ZOpacity);
-			})
-		}
-		// 让 li 从中间展开
-		move()
-		// 下一张，让轮播图动起来
-		function next() {
-			// 原理 ： 把数组中最后一个元素移到数组的第一位
-			states.unshift(states.pop());
-			move();
-		}
-		// 上一张
-		function prev() {
-			states.push(states.shift());
-			move();
-		}
-		// 点击下一张
-		$('.zy-slide .zy-next').click(function() {
-			next()
-		});
-		// 点击上一张
-		$('.zy-slide .zy-prev').click(function() {
-			prev()
-		});
 
-		// 自动轮播的方法
-		var interval = null;
-
-		function autoPlay() {
-			interval = setInterval(function() {
-				next();
-			}, setting.speed);
-		}
-		autoPlay();
-
-		// 停止轮播
-		$('.zy-slide section').hover(function() {
-			clearInterval(interval);
-		}, function() {
-			autoPlay();
-		})
-	}
-	// 找到要轮播图的根标签
+    // 找到要轮播图的根标签
 	$.fn.zySlide = function(options){
 		// options 传过来的参数
 		
 		$(this).each(function(i,ele){
 			console.log(options)
 			// 遍历出所有的 轮播图根标签
-			slide(ele,options);
+			// slide(ele,options);
+            new Slide(ele,options).showShuffling();
 		})
 		// 返回 元素
 		// 设置支持链式调用
 		return this;
 	}
-	
-})(jQuery)
+})(jQuery);
